4aac55857bfbf845ce69fd7903cf7e269cda7c7e
ReidMcLain <reidmm@gmail.com>
2022-10-20 21:56:20 -0500

    test to fix apostrophe

--- pages/about.js
+++ pages/about.js
@@ -4,13 +4,7 @@ import { Button, Text, Heading, Box, Link } from '@chakra-ui/react'
 const About = () => {
     return (
             <Heading p={100}>
-                Hello I'm Reid, and I'm Full Stack Software Developer. 
-                I graduated Auburn in 2020 with an Applied Mathematics degree. 
-                I'm 28 years old and live in Birmingham, AL. 
-                In January 2022 I joined a Software Bootcamp called Innovate Birmingham, and now have nearly a year of software development experience under my belt.
-                I am currently working at Icario as a Junior Software Developer.
-                So far the languages I've learned are HTML, CSS, Javascript, Python and JSX with a React framework and Chakra UI/UX Library. 
-                Prefer Linux Shell, work with agile/scrum practices, and am always, always learning.
+                Hi
             </Heading>
     )
 }

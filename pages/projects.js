2f89156d6ee2e5834e5cd63e78c66faed23ff324
ReidMcLain <reidmm@gmail.com>
2022-10-29 15:32:54 -0500

    removed a stack from projects

--- pages/projects.js
+++ pages/projects.js
@@ -4,38 +4,36 @@ import { Button, Text, Heading, Box, Link, Image, Center, Stack, Flex } from '@c
 import 'pure-react-carousel/dist/react-carousel.es.css';
 
 export default function Projects() {
-        return (
-            <Stack minH={'100vh'} direction={{ base: 'column', md: 'row' }}>
-                <Flex p={8} flex={1} align={'center'} justify={'center'}>
-                    <Stack spacing={6} w={'full'} maxW={'lg'}>
-                        <CarouselProvider
-                            naturalSlideWidth={700}
-                            naturalSlideHeight={700}
-                            totalSlides={3}
+    return (
+            <Flex p={8} flex={1} align={'center'} justify={'center'}>
+                <Stack spacing={6} w={'full'} maxW={'lg'} >
+                    <CarouselProvider
+                        naturalSlideWidth={700}
+                        naturalSlideHeight={700}
+                        totalSlides={3}
 
-                        >
-                            <Slider>
-                                <Slide index={0}>
-                                    <Image
-                                        src="https://images.unsplash.com/photo-1612852098516-55d01c75769a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDR8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=900&q=60"
-                                    />
-                                </Slide>
-                                <Slide index={1}>
-                                    <Image
-                                        src="https://images.unsplash.com/photo-1627875764093-315831ac12f7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDJ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=900&q=60"
-                                    />
-                                </Slide>
-                                <Slide index={2}>
-                                    <Image
-                                        src="https://images.unsplash.com/photo-1571432248690-7fd6980a1ae2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDl8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=900&q=60"
-                                    />
-                                </Slide>
-                            </Slider>
-                            <ButtonBack>Back</ButtonBack>
-                            <ButtonNext>Next</ButtonNext>
-                        </CarouselProvider>
-                    </Stack>
-                </Flex>
-                 </Stack>
-        )
-    }
+                    >
+                        <Slider>
+                            <Slide index={0}>
+                                <Image
+                                    src="https://images.unsplash.com/photo-1612852098516-55d01c75769a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDR8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=900&q=60"
+                                />
+                            </Slide>
+                            <Slide index={1}>
+                                <Image
+                                    src="https://images.unsplash.com/photo-1627875764093-315831ac12f7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDJ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=900&q=60"
+                                />
+                            </Slide>
+                            <Slide index={2}>
+                                <Image
+                                    src="https://images.unsplash.com/photo-1571432248690-7fd6980a1ae2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDl8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=900&q=60"
+                                />
+                            </Slide>
+                        </Slider>
+                        <ButtonBack>Back</ButtonBack>
+                        <ButtonNext>Next</ButtonNext>
+                    </CarouselProvider>
+                </Stack>
+            </Flex>
+    )
+}

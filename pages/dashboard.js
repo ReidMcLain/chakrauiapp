a28fc4a6e7c09b55bb2255d08f37941b774f8db6
ReidMcLain <reidmm@gmail.com>
2023-04-24 01:08:59 -0500

    added one more alt text to an image on dashboard.js

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -75,6 +75,7 @@ export default function SplitScreen() {
           src={
             'https://acegif.com/wp-content/uploads/2021/4fh5wi/welcome-15.gif'
           }
+          alt="Welcome"
         />
       </Flex>
     </Stack>
